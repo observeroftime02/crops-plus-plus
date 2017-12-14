8647adaa8f98908689ca02b58b254837a0599d80
LOKKO12 <33183715+LOKKO12@users.noreply.github.com>
2017-12-14 16:56:08 +0100

    Natura api fix

---
+++ main/java/mods/natura/blocks/crops/Glowshroom.java
@@ -0,0 +1,25 @@
+package mods.natura.blocks.crops;
+//Selfmade API possible due to License! TY!
+
+	import java.util.List;
+	import mods.natura.common.NContent;
+	import net.minecraft.block.Block;
+	import net.minecraft.block.BlockMushroom;
+	import net.minecraft.block.material.Material;
+	import net.minecraft.entity.player.EntityPlayer;
+	import net.minecraft.item.Item;
+	import net.minecraft.item.ItemBlock;
+	import net.minecraft.item.ItemStack;
+	import net.minecraft.util.IIcon;
+	import net.minecraft.util.StatCollector;
+	import cpw.mods.fml.relauncher.Side;
+	import cpw.mods.fml.relauncher.SideOnly;
+
+	public class Glowshroom extends BlockMushroom
+	{
+	    public Glowshroom()
+	    {
+	    	super();
+	
+	}
+}
